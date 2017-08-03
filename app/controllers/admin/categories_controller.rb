47e0962950587308e49826b9684d7641d090fa46
John Tolentino <jpftolentino@gmail.com>
2017-08-03 16:57:32 +0000

    Adding categories as admin working

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,30 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all.order(name: :asc)
+  end
+
+  # GET render /category/new
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new 
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+end
