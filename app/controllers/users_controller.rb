006e1729e9ce7866a3d287e315dde1fad8ee6843
John Tolentino <jpftolentino@gmail.com>
2017-08-05 07:02:21 +0000

    Fixed register bug where a user can signup with the same email, error messages now displaying

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      render :new
     end
   end
 
@@ -24,5 +25,10 @@ class UsersController < ApplicationController
         :password_confirmation
       )
     end
+
+    # def check_emai    `Al(email)
+
+    
+    # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
 
   has_many :reviews
 
+  validates :email, uniqueness: true
+  validates :password, confirmation: true 
 end
